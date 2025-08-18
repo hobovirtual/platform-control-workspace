4936bfb7d5a341804f654f7fae3846d36869d204
hobovirtual <christian.renaud@hashicorp.com>
2025-08-18 16:46:17 -0400

    think i get it

--- modules/github-repo-setup/main.tf
+++ modules/github-repo-setup/main.tf
@@ -7,9 +7,9 @@ locals {
 }
 
 resource "github_repository" "team_repo" {
-  count = length(var.ALL_REPO_NAMES)
-  name = var.ALL_REPO_NAMES[count.index]
-  description = var.ALL_REPO_DESCRIPTIONS[count.index]
+  count = length(local.ALL_REPO_NAMES)
+  name = local.ALL_REPO_NAMES[count.index]
+  description = local.ALL_REPO_DESCRIPTIONS[count.index]
   auto_init = true
   visibility = "public"
 }
--- modules/github-repo-setup/variables.tf
+++ modules/github-repo-setup/variables.tf
@@ -1,6 +1,4 @@
 # team defined varaibles
-variable "ALL_REPO_NAMES" {}
-variable "ALL_REPO_DESCRIPTIONS" {}
 variable "GH_APP_ID" {}
 variable "GH_APP_INSTALLATION_ID" {}
 variable "GH_ORGANIZATION" {}
