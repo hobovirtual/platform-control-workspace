d134eae0370fe466ca86480d1080175f3ea9cbe2
hobovirtual <christian.renaud@hashicorp.com>
2025-08-18 13:10:42 -0400

    test

--- variables.tf
+++ variables.tf
@@ -4,5 +4,8 @@ variable "TEAM_WS_NAME" {
 }
 
 # terraform variable declaration - definitions can be found in HCP TF
+variable "GH_APP_ID" {}
+variable "GH_APP_INSTALLATION_ID" {}
+variable "GH_ORGANIZATION" {}
 variable "TFE_ORG" {}
 variable "TFE_PROJECT" {}
