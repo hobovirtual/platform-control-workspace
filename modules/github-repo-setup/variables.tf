fb45d0c5cdb1a0524295b68e60c80b5277db0952
hobovirtual <christian.renaud@hashicorp.com>
2025-08-18 13:00:07 -0400

    init

--- modules/github-repo-setup/variables.tf
+++
@@ -1,4 +0,0 @@
-# terraform variable declaration - definitions can be found in HCP TF
-variable "GITHUB_APP_ID" {}
-variable "GITHUB_APP_INSTALLATION_ID" {}
-variable "GITHUB_ORGANIZATION" {}
--- variables.tf
+++ variables.tf
@@ -4,5 +4,8 @@ variable "TEAM_WS_NAME" {
 }
 
 # terraform variable declaration - definitions can be found in HCP TF
+variable "GITHUB_APP_ID" {}
+variable "GITHUB_APP_INSTALLATION_ID" {}
+variable "GITHUB_ORGANIZATION" {}
 variable "TFE_ORG" {}
 variable "TFE_PROJECT" {}
