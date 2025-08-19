98b345aa67e9e702ee435da8b006ec5c32c6285f
hobovirtual <christian.renaud@hashicorp.com>
2025-08-19 08:33:40 -0400

    removed for_each in favor of count

--- main.tf
+++ main.tf
@@ -15,8 +15,8 @@ data "tfe_project" "project" {
 
 resource "tfe_workspace" "team-ws" {
   count = length(var.TEAMS)
-  for_each = {for TEAM in var.TEAMS : TEAM.NAME => TEAM}
-  name = each.value.WS_NAME
+  #for_each = {for TEAM in var.TEAMS : TEAM.NAME => TEAM}
+  name = var.TEAMS[count.index].WS_NAME   #each.value.WS_NAME
   organization = var.TFE_ORG
   project_id = data.tfe_project.project.id
   auto_apply = true
@@ -34,8 +34,8 @@ resource "tfe_workspace" "team-ws" {
 
 module "vcs_setup" {
   source = "./modules/github-repo-setup"
-  REPO_NAME = var.TEAMS[count].REPO_NAME
-  REPO_DESCRIPTION = var.TEAMS[count].REPO_DESCRIPTION
+  REPO_NAME = var.TEAMS[count.index].REPO_NAME
+  REPO_DESCRIPTION = var.TEAMS[count.index].REPO_DESCRIPTION
   #TEAMS = var.TEAMS
   GH_APP_ID = var.GH_APP_ID
   GH_APP_INSTALLATION_ID = var.GH_APP_INSTALLATION_ID
